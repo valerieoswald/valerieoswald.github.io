629531c51bd1dfac288acd655448fb31596aa6d6
Valerie Oswald <valerie.oswald@student.uibk.ac.at>
2018-04-18 18:03:18 +0200

    layer hinzugefügt

--- basemap.js
+++ basemap.js
@@ -1,7 +1,28 @@
 
 let myMap = L.map("mapdiv");
-let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 
-myMap.addLayer(myLayer);
+let myLayers ={
+osm: L.tileLayer (
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+),
+geolandbasemap: L.tileLayer (
+    "https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"
+), 
+bmapoverlay: L.tileLayer (
+    "https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"
+),
+bmapgrau: L.tileLayer (
+    "https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"
+),
+bmaphidpi: L.tileLayer (
+    "https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"
+),
+bmaporthofoto30cm: L.tileLayer (
+    "https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"
+)
+};
+
+
+myMap.addLayer(myLayers.bmaporthofoto30cm);
 myMap.setView([47.267,11.383], 11);
 
